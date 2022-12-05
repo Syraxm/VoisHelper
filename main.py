6861adeac772e3ca0147f9ae97ebe220f4625bb1
Syraxm <tyomachannel@gmail.com>
2022-12-05 18:35:25 +0300

    eeee

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import speech_recognition as sr
-from googletrans import Translator
-from gtts import gTTS
-from playsound import playsound
+import os
+import sys
+import webbrowser
+import pyttsx3
+import speech_recognition
 
-mic = sr.Microphone(device_index=1)
-translator = Translator()
 
-r = sr.Recognizer()
-with mic as source:
-    print("Что перевести?: ")
-    tts = gTTS("Что вы хотите перевести?", lang="ru")
-    tts.save("output.mp3")
-    playsound("output.mp3")
-    audio = r.listen(source)
+def talk(words):
+    print(words)
+    engene = pyttsx3.init()
+    engene.say(words)
+    engene.runAndWait()
 
-try:
-    a = r.recognize_google(audio, language="ru-RU")
-    result = translator.translate(a, src='ru', dest='en').text
-    print(result)
-    tts = gTTS(result, lang="en")
-    tts.save("outputt.mp3")
-    playsound("outputt.mp3")
-except sr.UnknownValueError:
-    print("Говори по-человечески, плиз")
-except sr.RequestError as e:
-    print("Хз чё случилось")
+
+def command():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as sourse:
+        print("Вы можете говорить: ")
+        talk('Вы можете говорить')
+
